e9771108e577d02ad1b5342007fc5fdd9467837d
PeetV <peetjvermeulen@gmail.com>
2022-10-22 14:59:04 +1100

    Refactoring

--- Tests/UtilTests.cs
+++ Tests/UtilTests.cs
@@ -344,7 +344,7 @@ public class Statistics
     }
 
     [Fact]
-    public void Std()
+    public void StdevP()
     {
         var input = new double[] { 1, 2, 3, 4 };
         Assert.Equal(1.118033988749895, CsML.Util.Statistics.StdevP(input));
