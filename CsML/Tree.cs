70da43812c921e550d4b674ad2ff46ad5d3553b2
PeetV <peetjvermeulen@gmail.com>
2022-11-20 15:54:01 +1100

    File refactoring

--- CsML/Tree.cs
+++ CsML/Tree.cs
@@ -208,7 +208,7 @@ public class BinaryTree
                 throw new ArgumentException(CsML.Errors.Messages.E3);
             if (matrix.GetLength(1) != minColumns)
                 throw new ArgumentException(CsML.Errors.Messages.E4);
-            
+
         }
         Span2D<double> matrixSpan = matrix;
         var result = new double[inputRecordCount];
@@ -378,5 +378,248 @@ public class BinaryTree
         node.yesIndex = Grow(sm.Item1, st.Item1, depth);
         node.noIndex = Grow(sm.Item2, st.Item2, depth);
         return nodeIndex;
+    }
+}
+
+/// <summary>
+/// A forest of binary decision trees for classification and regression.
+/// </summary>
+public class RandomForest
+{
+    /// <summary>The trees that make up the forest once trained.</summary>
+    public List<BinaryTree> trees;
+
+    /// <summary>The number of tress in the forest.</summary>
+    public int treeCount;
+
+    /// <summary>
+    /// The number of matrix columns the model was trained on.
+    /// </summary>
+    public int minColumns;
+
+    /// <summary>The number of matrix rows the model was trained on.</summary>
+    public int inputRecordCount;
+
+    /// <summary>Maximum tree depth stopping condition.</summary>
+    public int maxdepth = 1000;
+
+    /// <summary>Minimum rows stopping condition.</summary>
+    public int minrows = 3;
+
+    /// <summary>
+    /// Number of random features to use at each split point during training.
+    /// Used to add randomisation. Defaults to square root of the number of
+    /// columns.</summary>
+    public int randomFeatures;
+
+    /// <summary>
+    /// The distinct class labels if creating a classification model.
+    /// </summary>
+    public double[]? classes;
+
+    /// <summary>
+    /// The function to use to calculate the purity of a slice of the target
+    /// array.<see> See
+    /// <seealso cref="CsML.Util.Statistics.Gini" />
+    /// for default function to use.
+    /// </see>
+    /// </summary>
+    public Func<double[], double> purityFn;
+
+    /// <summary>
+    /// Sample input data with replacement when training each tree if true.
+    /// </summary>
+    public bool bootstrapSampleData = true;
+
+    /// <summary>Keep out of bag indeces when bootstrap sampling.</summary>
+    public bool retainOutOfBagIndeces = false;
+
+    private string _mode;
+
+    /// <summary>Mode can be either "classify" or "regress".</summary>
+    public string Mode
+    {
+        get { return _mode; }
+        set
+        {
+            if (value != "classify" & value != "regress")
+                throw new ArgumentException(CsML.Errors.Messages.E5);
+            _mode = value;
+        }
+    }
+
+    /// <summary>Create an untrained random forest.</summary>
+    public RandomForest(string mode,
+        Func<double[], double> purityFn,
+        int treeCount = 103,
+        int randomFeatures = 0)
+    {
+        trees = new List<BinaryTree>();
+        minColumns = 0;
+        inputRecordCount = 0;
+        _mode = "classify";
+        this.randomFeatures = randomFeatures;
+        Mode = mode;
+        this.purityFn = purityFn;
+        this.treeCount = treeCount;
+    }
+
+    private int DefaultRandomFeatures(int n)
+    {
+        return (int)Math.Sqrt(n);
+    }
+
+    /// <summary>Train the model.</summary>
+    /// <param name="matrix">The features to train the model on.</param>
+    /// <param name="target">The target vector to train on.</param>
+    /// <exception cref="System.ArgumentException">
+    /// Thrown if inputs aren't the same length or empty.
+    /// </exception>
+    public void Train(double[,] matrix, double[] target)
+    {
+        inputRecordCount = matrix.GetLength(0);
+        int targetLength = target.Length;
+        if (inputRecordCount == 0 | targetLength == 0)
+            throw new ArgumentException(CsML.Errors.Messages.E1);
+        if (inputRecordCount != targetLength)
+            throw new ArgumentException(CsML.Errors.Messages.E2);
+        trees = new List<BinaryTree>();
+        minColumns = matrix.GetLength(1);
+        if (_mode == "classify")
+            classes = target.Distinct().ToArray();
+        randomFeatures = randomFeatures == 0 ?
+                         DefaultRandomFeatures(minColumns) :
+                         randomFeatures;
+        foreach (int cntr in Enumerable.Range(0, treeCount))
+        {
+            // Note that mode and purityFn is set in the constructor
+            var tree = new BinaryTree(this.Mode, this.purityFn);
+            tree.maxdepth = this.maxdepth;
+            tree.minrows = this.minrows;
+            tree.randomFeatures = this.randomFeatures;
+            tree.bootstrapSampleData = this.bootstrapSampleData;
+            tree.retainOutOfBagIndeces = this.retainOutOfBagIndeces;
+            trees.Add(tree);
+        }
+        Parallel.ForEach(trees, tree => tree.Train(matrix, target, true));
+    }
+
+    /// <summary>Make predictions using the model.</summary>
+    /// <param name="matrix">New data to infer predictions from.</param>
+    /// <exception cref="System.ArgumentException">
+    /// Thrown if input is empty, or model has not been trained, or if trained
+    /// on a different number of columns.
+    /// </exception>
+    public double[] Predict(double[,] matrix)
+    {
+        inputRecordCount = matrix.GetLength(0);
+        if (inputRecordCount == 0)
+            throw new ArgumentException(CsML.Errors.Messages.E1);
+        if (trees.Count == 0)
+            throw new ArgumentException(CsML.Errors.Messages.E3);
+        if (matrix.GetLength(1) != minColumns)
+            throw new ArgumentException(CsML.Errors.Messages.E4);
+        var result = new double[inputRecordCount];
+        Parallel.For(0, inputRecordCount, i =>
+        {
+            var input = new List<double[]>();
+            input = input.Append(
+                CsML.Util.Matrix.GetRow(matrix, i, false)).ToList();
+            if (Mode == "regress")
+            {
+                var predictions = new List<double>(trees.Count);
+                foreach (var tree in trees)
+                    predictions.Add(
+                        tree.Predict(
+                            CsML.Util.Matrix.FromListRows(input))[0]);
+                result[i] = predictions.Average();
+            }
+            else
+            {
+                var counts = new CsML.Probability.Counter<double>();
+                double vote;
+                foreach (var tree in trees)
+                {
+                    vote = tree.Predict(CsML.Util.Matrix.FromListRows(input))[0];
+                    counts.Increment(vote);
+                }
+                result[i] = counts.MaxKey();
+            }
+        });
+        return result;
+    }
+
+    /// <summary>
+    /// Predict labels for new data and return corresponding class probability
+    /// estimates.
+    /// </summary>
+    /// <exception cref="System.ArgumentException">
+    /// Thrown if model has not been trained, or input is empty or the model
+    /// has not been trained.
+    /// </exception>
+    public (double, Dictionary<double, double>)[] PredictWithProbabilities(
+        double[,] matrix,
+        bool skipchecks = false)
+    {
+        inputRecordCount = matrix.GetLength(0);
+        if (!skipchecks)
+        {
+            if (inputRecordCount == 0)
+                throw new ArgumentException(CsML.Errors.Messages.E1);
+            if (trees.Count == 0)
+                throw new ArgumentException(CsML.Errors.Messages.E3);
+            if (matrix.GetLength(1) != minColumns)
+                throw new ArgumentException(CsML.Errors.Messages.E4);
+            if (Mode == "regress")
+                throw new ArgumentException(CsML.Errors.Messages.E6);
+        }
+        var result = new (double, Dictionary<double, double>)[inputRecordCount];
+        Parallel.For(0, inputRecordCount, i =>
+        {
+            var input = new List<double[]>();
+            input = input.Append(
+                CsML.Util.Matrix.GetRow(matrix, i, false)).ToList();
+            var counts = new CsML.Probability.Counter<double>();
+            var probs = new Dictionary<double, double>();
+            (double, Dictionary<double, double>) vote;
+            foreach (var tree in trees)
+            {
+                vote = tree.PredictWithProbabilities(
+                    CsML.Util.Matrix.FromListRows(input))[0];
+                counts.Increment(vote.Item1);
+                foreach (double key in vote.Item2.Keys)
+                {
+                    if (probs.ContainsKey(key))
+                        probs[key] += vote.Item2[key];
+                    else
+                        probs[key] = vote.Item2[key];
+                }
+            }
+            double sumprobs = probs.Values.Sum();
+            foreach (double key in probs.Keys)
+                probs[key] = probs[key] / sumprobs;
+            result[i] = (counts.MaxKey(), probs);
+        });
+        return result;
+    }
+
+    /// <summary>
+    /// Calculate the mean weighted purity gains across trees.
+    /// </summary>
+    /// <returns>
+    /// An array containing purity gains in the order of feature columns.
+    /// </returns>
+    public double[] PurityGains()
+    {
+        var result = new double[minColumns];
+        foreach (var tree in trees)
+        {
+            double[] treeGains = tree.PurityGains();
+            for (int idx = 0; idx < minColumns; idx++)
+                result[idx] = result[idx] + treeGains[idx];
+        }
+        for (int idx = 0; idx < minColumns; idx++)
+            result[idx] = result[idx] / (double)trees.Count;
+        return result;
     }
 }
--- Tests/TreeTests.cs
+++ Tests/TreeTests.cs
@@ -330,3 +330,109 @@ public class BinaryTree
                     ttest, predictions) > 0.8);
     }
 }
+public class RandomForest
+{
+    private CsML.Tree.RandomForest BuildForestManually()
+    {
+        var tree = new CsML.Tree.BinaryTree("classify",
+                                            CsML.Util.Statistics.Gini);
+        tree.inputRecordCount = 20;
+        tree.minColumns = 2;
+        tree.classes = new double[] { 5, 6 };
+
+        var node1 = new CsML.Tree.BinaryNode();
+        node1.index = 0;
+        node1.yesIndex = 1;
+        node1.noIndex = 2;
+        node1.isLeaf = false;
+        node1.columnIndex = 0;
+        node1.splitPoint = 10;
+        node1.purityGain = 1.0;
+        tree.nodes.Add(node1);
+
+        var node2 = new CsML.Tree.BinaryNode();
+        node2.index = 1;
+        node2.isLeaf = true;
+        node2.recordCount = 20;
+        node2.classCounts = new Dictionary<double, int> { { 5, 5 }, { 6, 15 } };
+        node2.predicted = 5;
+        tree.nodes.Add(node2);
+
+        var node3 = new CsML.Tree.BinaryNode();
+        node3.index = 2;
+        node3.isLeaf = true;
+        node3.recordCount = 20;
+        node3.classCounts = new Dictionary<double, int> { { 5, 5 }, { 6, 15 } };
+        node3.predicted = 6;
+        tree.nodes.Add(node3);
+
+        var forest = new CsML.Tree.RandomForest("classify",
+                                                  CsML.Util.Statistics.Gini);
+        forest.minColumns = 2;
+        forest.trees = new List<CsML.Tree.BinaryTree> { tree, tree, tree };
+
+        return forest;
+    }
+
+    [Fact]
+    public void Predict()
+    {
+        CsML.Tree.RandomForest forest = BuildForestManually();
+        var data = new double[,] { { 11, 11 }, { 9, 9 } };
+        var predictions = forest.Predict(data);
+        Assert.True(new double[] { 5, 6 }.SequenceEqual(predictions));
+    }
+
+    [Fact]
+    public void PredictWithProbabilities()
+    {
+        CsML.Tree.RandomForest forest = BuildForestManually();
+        var data = new double[,] { { 11, 11 }, { 9, 9 } };
+        var predictions = forest.PredictWithProbabilities(data);
+        Assert.True(new double[] { 5, 6 }
+                .SequenceEqual(predictions.Select(x => x.Item1).ToArray()));
+        Assert.Equal(5.0 / 20.0, predictions[0].Item2[5.0]);
+        Assert.Equal(15.0 / 20.0, predictions[0].Item2[6.0]);
+    }
+
+    [Fact]
+    public void Train_predict_iris()
+    {
+        var mapping = new Dictionary<int, Dictionary<string, double>>();
+        mapping[4] = new Dictionary<string, double>
+           {
+               { "versicolor", 0 }, {"virginica", 1 }, {"setosa", 2}
+           };
+        string strWorkPath = Directory
+                                .GetParent(Environment.CurrentDirectory)!
+                                .Parent!
+                                .Parent!
+                                .FullName;
+        string inpuPath = Path.Combine(strWorkPath, "Data/iris.csv");
+        double[,] matrix = CsML.Util.Matrix.FromCSV(
+                                inpuPath, mapping, loadFromRow: 1);
+        Span2D<double> matrixSpan = matrix;
+        double[,] features = matrixSpan.Slice(0, 0, 150, 4).ToArray();
+        Assert.Equal(5.1, features[0, 0]);
+        Assert.Equal(3.5, features[0, 1]);
+        Assert.Equal(1.4, features[0, 2]);
+        Assert.Equal(0.2, features[0, 3]);
+        Assert.Equal(150, features.GetLength(0));
+        double[] target = matrixSpan.GetColumn(4).ToArray();
+        Assert.Equal(2, target[0]);
+        Assert.Equal(150, target.Length);
+
+        (features, target) = CsML.Util.Features.Shuffle(features, target);
+        double[,] ftrain, ftest;
+        double[] ttrain, ttest;
+        ((ftrain, ttrain), (ftest, ttest)) = CsML.Util.Features.Split(
+                                                features, target, 0.8);
+        var forest = new CsML.Tree.RandomForest("classify",
+                                                  CsML.Util.Statistics.Gini);
+        forest.Train(ftrain, ttrain);
+        Assert.True(forest.trees.Count > 0);
+        double[] predictions = forest.Predict(ftest);
+        Assert.True(CsML.Util.Array.ClassificationAccuracy(
+                    ttest, predictions) > 0.8);
+    }
+}
