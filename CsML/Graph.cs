89785301f73b9eef9cd01acec0bd6fcf2aaafd8c
PeetV <peetjvermeulen@gmail.com>
2022-11-29 19:13:34 +1100

    Edge interface

--- CsML/Graph.cs
+++ CsML/Graph.cs
@@ -1,23 +1,31 @@
 namespace CsML.Graph;
 
+/// <summary>Interface for weighted graph edges.</summary>
+public interface IEdge
+{
+    /// <summary>The edge weight.</summary>
+    double Weight { get; set; }
+}
+
 /// <summary>
 /// A graph with edges that have direction and weight and T as node type.
 /// </summary>
-public class DirectedWeightedGraph<T>
-    where T : notnull
+public class DirectedWeightedGraph<TNode, TEdge>
+    where TNode : notnull
+    where TEdge : IEdge
 {
-    private List<T> nodes;
+    private List<TNode> nodes;
 
     // Columns represent destinations (to) and rows sources (from). 0 in a cell
     // represents no link. A number other than 0 indicates a link and the
     // number represents the weight of the edge.
-    private List<List<int>> adjacencyMatrix;
+    private List<List<TEdge>> adjacencyMatrix;
 
     /// <summary>Create a new empty graph.</summary>
     public DirectedWeightedGraph()
     {
-        nodes = new List<T>();
-        adjacencyMatrix = new List<List<int>>();
+        nodes = new List<TNode>();
+        adjacencyMatrix = new List<List<TEdge>>();
     }
 
 }
