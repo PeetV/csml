af4c32e14725b8b2b5a9444a5e3ee5824815fe1f
PeetV <PeetV@users.noreply.github.com>
2023-01-01 17:32:07 +1100

    Update Program.cs

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+
+namespace CsML.Examples;
+
+public enum DataSet
+{
+    Iris,
+    Led,
+    Sonar
+}
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var dataSetOption = new Option<DataSet>(
+            name: "--dataset",
+            description: "Example dataset to use",
+            getDefaultValue: () => DataSet.Iris);
+
+        var rootCommand = new RootCommand("CsML examples");
+
+        var classifyCommand = new Command("classify", "Classification example.")
+            {
+                dataSetOption,
+            };
+        rootCommand.AddCommand(classifyCommand);
+
+        //readCommand.SetHandler(async (file, delay, fgcolor, lightMode) =>
+        //    {
+        //        await ReadFile(file!, delay, fgcolor, lightMode);
+        //    },
+        //    fileOption, delayOption, fgcolorOption, lightModeOption);
+
+        return rootCommand.InvokeAsync(args).Result;
+    }
+}
